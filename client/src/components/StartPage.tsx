0ce4b208670673e13e67f7fba52e1059759c1096
Victor Wei <victoryqwei@gmail.com>
2022-09-17 12:11:22 -0400

    fix: enter game button

--- client/src/components/StartPage.tsx
+++ client/src/components/StartPage.tsx
@@ -1,57 +1,45 @@
-import React from 'react'
-import SquareOutlinedIcon from '@mui/icons-material/SquareOutlined';
-import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
-import ChangeHistoryOutlinedIcon from '@mui/icons-material/ChangeHistoryOutlined';
+import React from "react";
+import SquareOutlinedIcon from "@mui/icons-material/SquareOutlined";
+import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
+import ChangeHistoryOutlinedIcon from "@mui/icons-material/ChangeHistoryOutlined";
 
-import './eye.css'
-import { useNavigate } from 'react-router';
+import "./eye.css";
+import { useNavigate } from "react-router";
 
-const logo = require('../assets/images/squidlogo.png')
+const logo = require("../assets/images/squidlogo.png");
 
 const StartPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = (e: any) => {
-        e.preventDefault();
-        navigate('/?path=/game')
-        //Implement switching to game using Link component?
-    }
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    navigate("/game");
+  };
   return (
-    <div className='rows'>
-        <div className='start-page'>
-
-        <div className='icon-wrapper' >
-            
-            <CircleOutlinedIcon className='sg-icons pinkcol' />
-            <ChangeHistoryOutlinedIcon className='sg-icons' />
-            <SquareOutlinedIcon className='sg-icons pinkcol' />
-
+    <div className="rows">
+      <div className="start-page">
+        <div className="icon-wrapper">
+          <CircleOutlinedIcon className="sg-icons pinkcol" />
+          <ChangeHistoryOutlinedIcon className="sg-icons" />
+          <SquareOutlinedIcon className="sg-icons pinkcol" />
         </div>
 
-        <img src={logo} className='logo-image'alt='Game Of Squid'/>
-            
-
+        <img src={logo} className="logo-image" alt="Game Of Squid" />
 
         <p>The Calamari Contest of Fun with Guns! Can you make it to the end? </p>
 
-        <button onClick={handleClick} className='play-button'>
-            Enter the Game 
+        <button onClick={handleClick} className="play-button">
+          Enter the Game
         </button>
-
-
-
-       
-        </div> 
-        <div className='eyeballs'>
-            <div className="wrap">
-                    <div className="eye double-blink"></div>
-                    <div className="eye double-blink"></div>
-                </div>
-            
+      </div>
+      <div className="eyeballs">
+        <div className="wrap">
+          <div className="eye double-blink"></div>
+          <div className="eye double-blink"></div>
         </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default StartPage
+export default StartPage;
