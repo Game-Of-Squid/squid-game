de13f9c168e36d5cde2b9dcd0a3544ccc646fd86
Alyzen Jeraj <alyzenjeraj@gmail.com>
2022-09-17 01:53:47 -0400

    Added Start Page

---
+++ client/src/components/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StartPage = () => {
+  return (
+    <div>
+        <h1>Game of Squids!</h1>
+        <p>A Game of Fun and Guns!</p>
+    </div>
+  )
+}
+
+export default StartPage
