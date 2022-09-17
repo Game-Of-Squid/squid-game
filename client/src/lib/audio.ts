b92c64a47c7cb90e0141e7232c134821c08016d6
Alyzen Jeraj <alyzenjeraj@gmail.com>
2022-09-17 19:04:36 -0400

    Changed sound files

--- client/src/lib/audio.ts
+++ client/src/lib/audio.ts
@@ -1,10 +1,10 @@
 export default function playAudio(greenLight: boolean) {
   if (greenLight) {
-    var audio = new Audio("http://localhost:3000/greenlight.mp3");
+    var audio = new Audio("http://localhost:3000/greenlightenglish.mp3");
     // let audio = new Audio("http://localhost:3000/koreangreen.mp3");
     audio.play();
   } else {
-    let audio = new Audio("http://localhost:3000/redlight.mp3");
+    let audio = new Audio("http://localhost:3000/redlightenglish.mp3");
     audio.play();
   }
 }
